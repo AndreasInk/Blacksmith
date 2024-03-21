c221bf77289777e6c2928bb5a16ffc0738b1d13b
Andreas Ink <andreasink17@gmail.com>
2024-03-20 23:39:10 -0400

    Update ExportSize.swift

--- Sources/Blacksmith/Helpers/ExportSize.swift
+++ Sources/Blacksmith/Helpers/ExportSize.swift
@@ -15,41 +15,53 @@ public enum ExportSize {
     
     case mac
     
+    case appleWatch_44mm
+    case appleWatch_40mm
+    
     case custom(CGSize, Double)
     
     public var size: CGSize {
         switch self {
-            case .iPhone_6_5_Inches:
-                return CGSize(width: 1242, height: 2688)
-            case .iPhone_5_5_Inches:
-                return CGSize(width: 1242, height: 2208)
-                
-            case .iPadPro_12_9_Inches:
-                return CGSize(width: 2048, height: 2732)
-                
-            case .mac:
-                return CGSize(width: 1280, height: 800)
-                
-            case .custom(let size, _):
-                return size
+        case .iPhone_6_5_Inches:
+            return CGSize(width: 1242, height: 2688)
+        case .iPhone_5_5_Inches:
+            return CGSize(width: 1242, height: 2208)
+            
+        case .iPadPro_12_9_Inches:
+            return CGSize(width: 2048, height: 2732)
+            
+        case .mac:
+            return CGSize(width: 1280, height: 800)
+            
+        case .appleWatch_44mm:
+            return CGSize(width: 368, height: 448)
+            
+        case .appleWatch_40mm:
+            return CGSize(width: 324, height: 394)
+            
+        case .custom(let size, _):
+            return size
         }
     }
     
     public var cornerRadius: Double {
         switch self {
-            case .iPhone_6_5_Inches:
-                return 20
-            case .iPhone_5_5_Inches:
-                return 24
-                
-            case .iPadPro_12_9_Inches:
-                return 40
-                
-            case .mac:
-                return 8
-                
-            case .custom(_, let radius):
-                return radius
+        case .iPhone_6_5_Inches:
+            return 20
+        case .iPhone_5_5_Inches:
+            return 24
+            
+        case .iPadPro_12_9_Inches:
+            return 40
+            
+        case .mac:
+            return 8
+            
+        case .appleWatch_44mm, .appleWatch_40mm:
+            return 0
+            
+        case .custom(_, let radius):
+            return radius
         }
     }
 }
